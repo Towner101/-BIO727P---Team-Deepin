1ac29694631964782ac37ba979a37118f87c6269
Lydia Obeng Bema <lydiaobeng@Lydias-MacBook-Air.local>
2024-02-21 13:04:07 +0000

    Updating html template

--- DeepinWebApp/app/__init__.py
+++ DeepinWebApp/app/__init__.py
@@ -1,9 +1,11 @@
+
 from flask import Flask
+from .routes.main import main  
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'qmuldeepinweb'
-
+    app.register_blueprint(main)
     return app
+
 
 
