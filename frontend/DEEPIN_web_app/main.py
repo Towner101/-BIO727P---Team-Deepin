a3c0fbcbe0c40ca240f047bd275776eed80636e2
lydiaobeng <138057130+lydiaobeng@users.noreply.github.com>
2024-02-21 10:05:53 +0000

    Delete frontend/DEEPIN_web_app directory

--- frontend/DEEPIN_web_app/main.py
+++
@@ -1,9 +0,0 @@
-# this file is used to run the webserver
-
-from website import create_app
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
--- frontend/DEEPIN_web_app/website/auth.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-
-#define the name of the Blueprint
-auth = Blueprint('auth',__name__)
