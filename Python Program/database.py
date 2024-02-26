d1533d22e59fc6a89b114816584185dd23e25d1e
Shakkthi0101 <bt23844@qmul.ac.uk>
2024-02-26 10:32:26 +0000

    adding database for integration

--- Python Program/database.py
+++ Python Program/database.py
@@ -7,7 +7,10 @@ def get_db_connection():
 def get_snp_data(snp_id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM snps WHERE id=?", (snp_id,))
+    cursor.execute("SELECT * FROM SIBStats WHERE snpstatsID=?", (snp_id,))
     result = cursor.fetchone()
     conn.close()
     return result
+
+test=get_snp_data('1:10420:A:C')
+print(test)
